8e705290f421a871b2c15e6d9465ca10d80ecac8
Amarudinn <masamar707@gmail.com>
2025-12-29 16:44:45 +0700

    update

--- contracts/interfaces/IStakingReceiver.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IStakingReceiver {
-    function receiveRevenue() external payable;
-    function totalStaked() external view returns (uint256);
-}
